44aa802c3d1c3d2e2b2d2d45fd90bc5b898f9527
junmin-chang <chjm219@kookmin.ac.kr>
2021-11-17 14:15:15 +0900

    firebase 설정 in service folder

---
+++ src/service/firebase.ts
@@ -0,0 +1,32 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID,
+};
+
+// 초기화
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+// google auth
+export const signInGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return auth.signInWithPopup(provider);
+};
+
+// github auth
+export const signInGithub = () => {
+  const provider = new firebase.auth.GithubAuthProvider();
+  return auth.signInWithPopup(provider);
+};
+
+export const signOut = () => {
+  return auth.signOut();
+};
