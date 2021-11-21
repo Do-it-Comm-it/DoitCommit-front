4d1ebe17e4306e3d275869538f60997a31cd7f50
Yuuun <84560867+seoyuuun@users.noreply.github.com>
2021-11-21 13:16:13 +0900

    add "import githubButton"

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import GithubLoginBtn from '@src/components/GithubLogin';
 
 const Login = () => {
   return <h2>Login page</h2>;
