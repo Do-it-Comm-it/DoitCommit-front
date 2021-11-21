ddced7d068a0ee0678b01d6db0238ddcc933f730
Yuuun <84560867+seoyuuun@users.noreply.github.com>
2021-11-21 13:13:50 +0900

    update github-login-button code

--- src/components/GithubLogin/index.tsx
+++ src/components/GithubLogin/index.tsx
@@ -1,1 +1,10 @@
 // Github Login Button Component
+import React from "react";
+import {signInGithub} from "@src/service/firebase";
+import GithubButton from "react-github-login";
+
+const GithubLogin = () => {
+  return <GithubButton label="Sign In With Github" type="dark" onClick="{signInGithub}" />;
+};
+
+export default GithubLogin;
