5120fa7961354f2fd56e75f68a31c6d2cb4a3b3f
junmin-chang <chjm219@kookmin.ac.kr>
2021-12-02 17:55:31 +0900

    useDarkMode 훅 작성 / atom effect를 이용한 로컬스토리지에 theme 저장(for persisting theme data)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,17 +8,20 @@ import LoginModal from '@src/components/Organisms/LoginModal';
 import RegisterModal from '@src/components/Organisms/RegisterModal';
 import DIText from '@src/components/Atoms/DIText';
 import styled from 'styled-components';
+import useDarkMdoe from '@src/hooks/useDarkMode';
 const Home = () => {
   const { user, loading } = useAuthentication();
   const history = useHistory();
   const [modal, setModal] = useRecoilState(modalAtom);
+  const { theme, toggleTheme } = useDarkMdoe();
 
   useEffect(() => {
     // when user joined first.
+    console.log(theme); // check theme
     if (user && !user.isEnrolled) {
       setModal({ id: 'register', visible: true });
     }
-  }, [user, history, setModal]);
+  }, [user, history, setModal, theme]);
 
   const onClickLogin = useCallback(() => {
     setModal({ id: 'login', visible: true });
@@ -29,12 +32,14 @@ const Home = () => {
   }, [setModal]);
 
   return (
+    // <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme }> 이런 식?..
     <Container>
       {loading === true ? (
         <EmptyScreen />
       ) : (
         <Header>
           <DIText>Home</DIText>
+          <button onClick={toggleTheme}>Toggle Theme</button>
           <Wrapper>
             <DIText>안녕하세요 {user?.nickname ?? 'Stranger'}</DIText>
             {user === null && <button onClick={onClickLogin}>Login</button>}
