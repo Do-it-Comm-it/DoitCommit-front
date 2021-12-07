97bdb138e0405a4b6e44a02b7f079199f538c948
seoyuun <carpedm1221@gmail.com>
2021-12-08 02:35:27 +0900

    try to make sidebar 1st

---
+++ src/components/Organisms/Sidebar/Sidebar.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import { SidebarData } from "./SidebarData";
+
+function Sidebar() {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  return (
+    <>
+      <div
+        className={sidebar ? "sidebar-container active" : "sidebar-container"}
+      >
+        <ul className="sidebar-items">
+          <li className="sidebar-toggle">
+            <Link to="#" className="nav-menu-icon" onClick={showSidebar}>
+              <FaIcons.FaWindowClose />
+            </Link>
+          </li>
+          {SidebarData.map((sidebaritem) => {
+            return (
+              <li
+                key={sidebaritem.id}
+                className={sidebaritem.cName}
+                onClick={showSidebar}
+              >
+                <Link to={sidebaritem.path}>
+                  {sidebaritem.icon}
+                  <span>{sidebaritem.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
---
+++ src/components/Organisms/Sidebar/SidebarData.tsx
@@ -0,0 +1,18 @@
+import * as FaIcons from "react-icons/fa";
+
+export const SidebarData = [
+  {
+    id: 1,
+    title: "Home",
+    cName: "sidebar-item",
+    icon: <FaIcons.FaHome />,
+    path: "/",
+  },
+  {
+    id: 2,
+    title: "MyPage",
+    cName: "sidebar-item",
+    icon: <FaIcons.FaRegUser />,
+    path: "/myPage",
+  }
+];
