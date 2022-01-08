4ae4588eb00c1b691a42ca53c14cf1f4dd5b0293
z00mni <65433256+junmin-Chang@users.noreply.github.com>
2022-01-08 23:08:28 +0900

    axios response interceptor for refreshToken

--- client.tsx
+++ client.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from '@src/layouts/App';
 import { RecoilRoot } from 'recoil';
+import { axiosResponseInterceptor } from '@src/utils/fetcher';
 
+axiosResponseInterceptor();
 render(
   <RecoilRoot>
     <App />
--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,8 +1,15 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 const apiUrl = process.env.API_URL ?? 'http://localhost:8888';
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
+  withCredentials: true, // for cookie
+});
 
 export const requestAPI = (token?: string | null) => {
   const authHeader = (token: string | null) => {
@@ -54,3 +61,30 @@ export const requestAPI = (token?: string | null) => {
 
 //axios refreshtoken controller by intercpetors.
 axiosInstance.interceptors.request.use();
+
+// Login -> refresh Token , accessToken to Cookie, but when accessToken expires,
+// new request need to be prepared with new accessToken.
+// requset -> 401 Unauthorized (accessToken expires) -> request new accessToken -> request previous ones
+// with newly earned accessToken
+
+export const axiosResponseInterceptor = () => {
+  const interceptor = axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (err: AxiosError) => {
+      // 401 Unauthorized가 아닌 다른 에러라면
+      if (err.response?.status !== 401) {
+        return Promise.resolve(err);
+      }
+      // 무한 루프 방지 eject
+      axiosInstance.interceptors.response.eject(interceptor);
+      return axiosInstance
+        .get('/refresh') // 임의로 정해둔 새로운 accessToken을 발급받기 위한 컨트롤러
+        .then(() => {
+          return axiosInstance(err.response?.config!); // 이전 request 재요청
+        })
+        .catch((err) => {
+          return Promise.reject(err);
+        });
+    },
+  );
+};
