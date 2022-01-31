9d7e2088e72e51867411a352429f456801ae49e1
z00mni <chjm219@kookmin.ac.kr>
2022-01-31 21:27:38 +0900

    Tech 타입 따로 분기 및 User 타입 백단에 맞게 수정

--- src/typings/User.ts
+++ src/typings/User.ts
@@ -1,11 +1,11 @@
 export interface IUser {
   nickname?: string | null; // 초기정보 등록 전 까진 null
-  tech?: string[] | null; // 기술스택
+  interestTechSet?: string[] | null; // 기술스택
   position?: string | null; // 직군
   pictureUrl?: string | null;
   email: string | null;
   url1?: string | null;
   url2?: string | null;
   githubUrl?: string | null;
-  username?: string | null; // 아마 uuid 인듯?
+  regDate: Date;
 }
