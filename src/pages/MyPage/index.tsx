04bfa81afd8dce960efd2e30cffbea318cf1ccc7
z00mni <chjm219@kookmin.ac.kr>
2022-02-21 22:57:23 +0900

    search icon 사이즈 조절을 위해 width, height 프로퍼티 추가

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,5 +1,6 @@
 import Footer from '@src/components/Organisms/Footer';
 import Body from '@src/components/Organisms/MyPage/Body';
+import { devices } from '@src/utils/theme';
 import React from 'react';
 import styled from 'styled-components';
 const MyPage = () => {
@@ -16,8 +17,18 @@ const MyPage = () => {
 const Container = styled.section`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  width: 100%;
   height: 100%;
+  padding-top: 70px;
+  padding-left: 380px;
+  padding-right: 380px;
+  padding-bottom: 20px;
+
+  @media ${devices.laptopL} {
+    padding: 8%;
+    padding-left: 12%;
+    padding-right: 12%;
+  }
 `;
 
 export default MyPage;
