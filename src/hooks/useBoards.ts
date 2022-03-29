a497a9b05ff609568f09df20ad7d9e23c3dfde7e
z00mni <chjm219@kookmin.ac.kr>
2022-03-29 18:51:38 +0900

    Refactor: 뮤테이션 추출

--- src/hooks/useBoards.ts
+++ src/hooks/useBoards.ts
@@ -1,5 +1,6 @@
 import { board } from '@src/service/api';
-import { useInfiniteQuery } from 'react-query';
+import { IBoard } from '@src/typings/Board';
+import { useInfiniteQuery, useMutation, useQueryClient } from 'react-query';
 
 export const useBoards = () => {
   const fetchPosts = async ({ pageParam = 0 }) => {
@@ -38,4 +39,70 @@ export const useBoards = () => {
     fetchNextPage,
     isFetchingNextPage,
   };
+};
+
+export const useBoardListMutation = (fieldToEdit: Partial<IBoard>, api: any) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(api, {
+    onMutate: async (selectedBoard: IBoard) => {
+      await queryClient.cancelQueries('boards');
+      const snapshot = queryClient.getQueryData('boards');
+      queryClient.setQueryData('boards', (old: any) => {
+        return {
+          ...old,
+          pages: old.pages.map((page: any) => {
+            return {
+              ...page,
+              data: page.data.map((board: IBoard) => {
+                if (board.boardId === selectedBoard.boardId) {
+                  return {
+                    ...board,
+                    ...fieldToEdit,
+                  };
+                } else {
+                  return {
+                    ...board,
+                  };
+                }
+              }),
+            };
+          }),
+        };
+      });
+
+      // Return a snapshot so we can rollback in case of failure
+      return {
+        snapshot,
+      };
+    },
+    onSuccess() {
+      queryClient.invalidateQueries('boards');
+    },
+  });
+
+  return mutation;
+};
+
+export const useSingleBoardMutation = (fieldToEdit: Partial<IBoard>, api: any) => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(api, {
+    onMutate: async (selectedBoard: IBoard) => {
+      await queryClient.cancelQueries(`board/${selectedBoard.boardId}`);
+      const snapshot = queryClient.getQueryData(`board/${selectedBoard.boardId}`);
+      queryClient.setQueryData(`board/${selectedBoard.boardId}`, (old: any) => {
+        return {
+          ...old,
+          ...fieldToEdit,
+        };
+      });
+
+      return {
+        snapshot,
+      };
+    },
+    onSuccess() {
+      queryClient.invalidateQueries(`board/${fieldToEdit.boardId}`);
+    },
+  });
+  return mutation;
 };
