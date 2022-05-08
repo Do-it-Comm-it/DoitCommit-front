5faa9dbaf1f310dc80716eacfff58b5f552509f3
HYOJIN <38070150+alexrider94@users.noreply.github.com>
2022-05-08 21:28:57 +0900

    prettier 적용

--- src/service/date.ts
+++ src/service/date.ts
@@ -1,12 +1,12 @@
-const dateFormat = (date:string) => {
+const dateFormat = (date: string) => {
   // date : 2022-05-05T16:55
   if (date && date.includes('T')) {
     return date.replaceAll('-', '.').replace('T', ' ');
   }
-}
+};
 
 const dateList = {
-  dateFormat
-}
+  dateFormat,
+};
 
 export default dateList;
