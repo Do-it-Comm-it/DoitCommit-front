30b2a1522d4a30c93f57d728af48acb100370bf7
gbwlxhd97 <gbwlxhd97@naver.com>
2022-11-02 01:05:57 +0900

    feat : useScrollTop

--- src/components/Organisms/Board/Body.tsx
+++ src/components/Organisms/Board/Body.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import Notice from './Notice';
 import Articles from './Articles';
@@ -7,13 +7,15 @@ import BoardEditor from './BoardEditor';
 import Board from './Board';
 import { useUser } from '@src/hooks/useAuthentication';
 import { Route, Routes, useLocation } from 'react-router-dom';
+import FloatingScrollButton from './FloatingScrollButton';
+import useScrollTop from '@src/hooks/useScrollTop';
 
 type BoardPathType = 'notice' | 'edit' | 'board' | 'index';
 
 const Body = () => {
   const { data: user } = useUser();
   const location = useLocation();
-
+  const { showButton, onScroll } = useScrollTop(700);
   const path: BoardPathType = useMemo(() => {
     const name = location.pathname.split('/')[2];
 
@@ -39,6 +41,7 @@ const Body = () => {
       </Routes>
 
       {user && path !== 'edit' && path !== 'board' && <FloatingButton />}
+      {showButton && <FloatingScrollButton onScroll={onScroll} />}
     </Container>
   );
 };
